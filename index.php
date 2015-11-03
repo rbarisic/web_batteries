7d72747d13c4bdfb54a89be5750149097d17e032
Robert Barisic <rbarisic@Roberts-MacBook-Pro.local>
2015-11-03 19:50:39 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Insert Sitename</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/vendor/purecss/0.6.0/pure-min.css">
+    <link rel="stylesheet" type="text/css" href="assets/design.css">
+
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="assets/css/vendor/purecss/0.6.0/grids-responsive-old-ie-min.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="assets/css/vendor/purecss/0.6.0/grids-responsive-min.css">
+    <!--<![endif]-->
+
+    <script type="text/javascript" src="assets/js/vendor/jquery/2.1.4/jquery-2.1.4.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
